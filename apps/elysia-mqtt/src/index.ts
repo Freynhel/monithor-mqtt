82a77448e6bc1c436b6df1cbf269ba50cc2be003
Matheus Grendene <50559132+MatheusGrendene@users.noreply.github.com>
2026-03-06 17:49:27 -0300

    feat: add CORS support to Elysia app and update dependencies

--- apps/elysia-mqtt/src/index.ts
+++ apps/elysia-mqtt/src/index.ts
@@ -1,10 +1,17 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 import { generatorController } from "./modules/generator/index";
 
 const app = new Elysia()
-	.use(generatorController)
-	.listen(3001);
+  .use(
+    cors({
+      origin: "http://localhost:3000",   // only this origin is allowed
+      credentials: true,                
+    })
+  )
+  .use(generatorController)
+  .listen(3001);
 
 console.log(
-	`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
